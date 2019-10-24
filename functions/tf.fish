6a0989450ae7e881fe3a8642b51994f6a84caa5d
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-10-24 15:44:24 -0600

    update tf func

--- functions/tf.fish
+++ functions/tf.fish
@@ -1,11 +1,49 @@
 function tf --description 'wrapper for terraform'
-  if [ $argv[1] == "clean" ]
-    rm -rf .terraform
-  else
-    if set -q AWS_VAULT
-      terraform $argv
-    else
-      aws-vault exec default -- terraform $argv
+  switch($argv[1])
+  case a apply
+    terraform apply $argv[2..-1]
+  case p plan
+    terraform plan $argv[2..-1]
+  case po
+    terraform plan --out $argv[2..-1]
+  case s state
+    switch($argv[2])
+    case l list
+      terraform state list $argv[3..-1]
+    case m mv move
+      terraform state mv $argv[3..-1]
+    case pl pull
+      terraform state pull $argv[3..-1]
+    case ps push
+      terraform state push $argv[3..-1]
+    case r rm
+      terraform state rm $argv[3..-1]
+    case s show
+      terraform state show $argv[3..-1]
     end
+  case f fmt
+    terraform fmt $argv[2..-1]
+  case i init
+    terraform init $argv[2..-1]
+  case w work workspace
+    switch ($argv[2])
+    case n new
+      terraform workspace new $argv[3..-1]
+    case l list
+      terraform workspace list $argv[3..-1]
+    case d del delete
+      terraform workspace delete $argv[3..-1]
+    case co ch sel select
+      terraform workspace select $argv[3..-1]
+    case s sh show
+      terraform workspace show $argv[3..-1]
+    end
+  case v ver version
+    terraform version $argv[2..-1]
+  case va val validate
+    terraform validate $argv[2..-1]
+  case clean
+    rm -rf .terraform
+    rm -f *.plan
   end
 end
