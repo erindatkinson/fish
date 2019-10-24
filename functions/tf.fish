0cc944de714d065708e7bfa99923c8e4aa5da5ef
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-10-24 16:18:51 -0600

    for the reals tf update

--- functions/tf.fish
+++ functions/tf.fish
@@ -1,48 +1,48 @@
 function tf --description 'wrapper for terraform'
-  switch($argv[1])
-  case a apply
+  switch $argv[1]
+  case "a" "apply"
     terraform apply $argv[2..-1]
-  case p plan
+  case "p" "plan"
     terraform plan $argv[2..-1]
-  case po
+  case "po"
     terraform plan --out $argv[2..-1]
-  case s state
-    switch($argv[2])
-    case l list
+  case "s" "state"
+    switch $argv[2]
+    case "l" "list"
       terraform state list $argv[3..-1]
-    case m mv move
+    case "m" "mv" "move"
       terraform state mv $argv[3..-1]
-    case pl pull
+    case "pl" "pull"
       terraform state pull $argv[3..-1]
-    case ps push
+    case "ps" "push"
       terraform state push $argv[3..-1]
-    case r rm
+    case "r" "rm"
       terraform state rm $argv[3..-1]
-    case s show
+    case "s" "show"
       terraform state show $argv[3..-1]
     end
-  case f fmt
+  case "f" "fmt"
     terraform fmt $argv[2..-1]
-  case i init
+  case "i" "init"
     terraform init $argv[2..-1]
-  case w work workspace
-    switch ($argv[2])
-    case n new
+  case "w" "work" "workspace"
+    switch $argv[2]
+    case "n" "new"
       terraform workspace new $argv[3..-1]
-    case l list
+    case "l" "list"
       terraform workspace list $argv[3..-1]
-    case d del delete
+    case "d" "del" "delete"
       terraform workspace delete $argv[3..-1]
-    case co ch sel select
+    case "co" "ch" "sel" "select"
       terraform workspace select $argv[3..-1]
-    case s sh show
+    case "s" "sh" "show"
       terraform workspace show $argv[3..-1]
     end
-  case v ver version
+  case "v" "ver" "version"
     terraform version $argv[2..-1]
-  case va val validate
+  case "va" "val" "validate"
     terraform validate $argv[2..-1]
-  case clean
+  case "clean"
     rm -rf .terraform
     rm -f *.plan
   end
