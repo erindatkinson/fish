281213a2a6ed80d03cdb29e1b7029f9b44ad3aae
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2022-11-09 13:44:16 -0700

    update brewfile and pandoc func

--- functions/pandocs.fish
+++ functions/pandocs.fish
@@ -1,12 +1,12 @@
 function pandocs
   set -l fname (string split . $argv[2])[1]
   switch $argv[1]
-    case 'html' 
+    case 'html'
       pandoc -s -c ~/hashicorp/static/css/local/main.css -o "$fname.html" $argv[2]
     case 'pdf'
-      pandoc --pdf-engine=xelatex -o "$fname.pdf" $argv[2]
+      pandoc --pdf-engine=pdflatex -o "$fname.pdf" $argv[2]
     case '*'
       echo "error: the command '$argv[1]' does not exist"
-  
+
   end
 end
